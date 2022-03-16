54f95dec1b98d6ba8ce0c15ee7ed432b6686ece2
iamkunkel <iamkunkel@gmail.com>
2022-03-15 21:15:07 -0400

    init commit

---
+++ Assets/Scripts/ComputerPaddle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerPaddle : Paddle
+{
+    private void FixedUpdate()
+    {
+        
+    }
+}
